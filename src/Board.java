89ee781dfca5a3c49b68c0c11fa79be3aa5bbda1
Rachel Pierce <rachelpierce@students.berkeley.net>
2019-04-09 13:21:40 -0700

    4/9

--- src/Board.java
+++ src/Board.java
@@ -28,21 +28,18 @@ public class Board {
 
     boolean leftRight(String player){
         boolean a=false;
-        int counter=0;
-        for(int h=0;h<3;h++){
-            for(int w=0;w<3;w++){
+        for(int h=0;h<4;h++){
+            for(int w=0;w<4;w++){
                 if (grid[w][h].equals(player)){
                     if (grid[w+1][h].equals(player)){
                         if (grid[w+2][h].equals(player)){
                             if (grid[w+3][h].equals(player)){
-                                counter++;
+                                a=true;
+                                break;
                             }
                         }
                     }
                 }
-            }if(counter==4){
-                    a=true;
-                    break;
             }
         }
         return a;
@@ -50,21 +47,17 @@ public class Board {
 
     boolean upDown(String player) {
         boolean a=false;
-        int counter=0;
-        for (int w=0;w<=3;w++) {
-            for (int h=0;h<=3;h++) {
+        for (int w=0;w<=4;w++) {
+            for (int h=0;h<=4;h++) {
                 if (grid[w][h].equals(player)){
                     if (grid[w][h+1].equals(player)){
                         if (grid[w][h+2].equals(player)){
-                            if (grid[w][h+3].equals(player)){
-                                counter++;
+                            if (grid[w+3][h].equals(player)){
+                                a=true;
+                                break;
                             }
                         }
                     }
-                }
-                if (counter==4) {
-                    a=true;
-                    break;
                 }
             }
         }
@@ -78,12 +71,11 @@ public class Board {
             System.out.println("That is not a valid column");
             int colO=sc.nextInt();
             dropX(colO);
-        }
-        if (full[col-1]!=0){
+        }else if (grid[7][col]==" o"||grid[7][col]==" x"){
             System.out.println("That column is full, please choose another one");
             int colO=sc.nextInt();
             dropX(colO);
-        } else{
+        }else{
             for (int i=0;i<height;i++){
                 if (grid[bottomrow][col]==" ."){
                     grid[bottomrow][col]=" o";
@@ -96,37 +88,30 @@ public class Board {
                 }
             }
             if (upDown("o") == true || leftRight("o") == true) {
-                System.out.println("Yay player o has won!");
-                boolean game = false;
+                game = false;
             }
         }
     }
     void dropX(int col) {
         int bottomrow=0;
-        int counter=1;
         if (col>width){
             System.out.println("That is not a valid column");
             int colX=sc.nextInt();
             dropX(colX);
+        }else if (grid[7][col]==" o"||grid[7][col]==" x"){
+            System.out.println("That column is full, please choose another one");
+            int colX = sc.nextInt();
+            dropX(colX);
         }else{
-            if (full[col+1]!= 0) {
-                System.out.println("That column is full, please choose another one");
-                int colX=sc.nextInt();
-                dropX(colX);
-            }
             for (int i = 0; i < height; i++) {
-                if (grid[bottomrow][col]==" .") {
+                if (grid[bottomrow][col]==" ."){
                     grid[bottomrow][col]=" x";
                     break;
                 } else if (grid[bottomrow][col]==" o"||grid[bottomrow][col]==" x") {
                     bottomrow++;
-                    if (i==8) {
-                        full[col+1]=col;
-                    }
                 }
             }
             if (upDown("x") == true || leftRight("x") == true) {
-                System.out.println("Yay player x has won!");
                 game = false;
             }
         }
--- src/Game.java
+++ src/Game.java
@@ -3,19 +3,30 @@ public class Game{
     static public void main(String[]args){
         Board b=new Board();
         Scanner sc = new Scanner(System.in);
-        System.out.println("Choose a column to play. Column:1-8");
+        System.out.println("To play choose a column to play. Column:0-7");
+        System.out.println("Player 1's Turn");
+        b.PrintBoard();
         while(b.game==true){
            if(b.game==true){
                 int colX=sc.nextInt();
                 b.dropX(colX);
                 b.PrintBoard();
-                System.out.println("Player 2's Turn");
+               if (b.game==true){
+                   System.out.println("Player 2's Turn");
+               }else{
+                   System.out.println("Yay player x has won!");
+               }
+
             }
             if (b.game==true){
                 int colO=sc.nextInt();
                 b.dropO(colO);
                 b.PrintBoard();
-                System.out.println("Player 1's Turn");
+                if (b.game==true){
+                    System.out.println("Player 1's Turn");
+                }else{
+                    System.out.println("Yay player o has won!");
+                }
             }
         }
     }
